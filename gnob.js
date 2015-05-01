c2a0f19e60f59497644ca152cd1863b3f282c05a
hacknightly <hacknightly@gmail.com>
2015-05-01 17:06:24 -0600

    strict JS; Dont auto-initialize

--- gnob.js
+++ gnob.js
@@ -1,35 +1,37 @@
+'use strict';
 // Thanks Mozilla! https://developer.mozilla.org/en-US/docs/Web/Events/wheel
-// creates a global "addWheelListener" method
+// creates a global 'addWheelListener' method
 // example: addWheelListener( elem, function( e ) { console.log( e.deltaY ); e.preventDefault(); } );
 (function(window, document) {
+  'use strict';
 
-  var prefix = "",
+  var prefix = '',
     _addEventListener, onwheel, support;
 
   // detect event model
   if (window.addEventListener) {
-    _addEventListener = "addEventListener";
+    _addEventListener = 'addEventListener';
   } else {
-    _addEventListener = "attachEvent";
-    prefix = "on";
+    _addEventListener = 'attachEvent';
+    prefix = 'on';
   }
 
   // detect available wheel event
-  support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
-    document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
-    "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
+  support = 'onwheel' in document.createElement('div') ? 'wheel' : // Modern browsers support 'wheel'
+    document.onmousewheel !== undefined ? 'mousewheel' : // Webkit and IE support at least 'mousewheel'
+    'DOMMouseScroll'; // let's assume that remaining browsers are older Firefox
 
   window.addWheelListener = function(elem, callback, useCapture) {
     _addWheelListener(elem, support, callback, useCapture);
 
     // handle MozMousePixelScroll in older Firefox
-    if (support == "DOMMouseScroll") {
-      _addWheelListener(elem, "MozMousePixelScroll", callback, useCapture);
+    if (support == 'DOMMouseScroll') {
+      _addWheelListener(elem, 'MozMousePixelScroll', callback, useCapture);
     }
   };
 
   function _addWheelListener(elem, eventName, callback, useCapture) {
-    elem[_addEventListener](prefix + eventName, support == "wheel" ? callback : function(originalEvent) {
+    elem[_addEventListener](prefix + eventName, support == 'wheel' ? callback : function(originalEvent) {
       !originalEvent && (originalEvent = window.event);
 
       // create a normalized event object
@@ -37,8 +39,8 @@
         // keep a ref to the original event object
         originalEvent: originalEvent,
         target: originalEvent.target || originalEvent.srcElement,
-        type: "wheel",
-        deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
+        type: 'wheel',
+        deltaMode: originalEvent.type == 'MozMousePixelScroll' ? 0 : 1,
         deltaX: 0,
         deltaZ: 0,
         preventDefault: function() {
@@ -49,7 +51,7 @@
       };
 
       // calculate deltaY (and deltaX) according to the event
-      if (support == "mousewheel") {
+      if (support == 'mousewheel') {
         event.deltaY = -1 / 40 * originalEvent.wheelDelta;
         // Webkit also support wheelDeltaX
         originalEvent.wheelDeltaX && (event.deltaX = -1 / 40 * originalEvent.wheelDeltaX);
@@ -70,8 +72,6 @@ var NATURAL_OFF_DEG = -190;
 var NATURAL_MAX_POS = 295;
 
 var Gnob = function(rangeElem) {
-  var _this = this;
-  
   this.settings = this.getSettings(rangeElem);
   this.min      = (this.settings.min || 0);
   this.max      = (this.settings.max || 0) + 1;
@@ -83,7 +83,7 @@ var Gnob = function(rangeElem) {
   this.ticks    = this.getTicks();
 
   this.createKnob(rangeElem);
-  
+
   this.setValue(this.initial);
 };
 
@@ -107,7 +107,7 @@ Gnob.prototype.getSettings = function(rangeElem) {
 
 Gnob.prototype.setValue = function(value) {
   value = parseInt(value, 10);
-  
+
   if (value > this.max) {
     value = this.max - 1;
   }
@@ -117,20 +117,20 @@ Gnob.prototype.setValue = function(value) {
   else if (!value && value !== 0) {
     return false;
   }
-  
+
   var position = NATURAL_OFF_DEG + this.ticks[value];
 
   this.position = this.ticks[value];
   this.rangeElem.value = position;
   this.setRotation(position);
   this.input.value = value;
-  
+
   this.rangeElemOnChange(value);
 };
 
 Gnob.prototype.setRotation = function(value) {
   var inner = this.knobElem.children[0];
-  
+
   inner.style.transform = 'rotate(' + value + 'deg)';
 };
 
@@ -139,25 +139,23 @@ Gnob.prototype.rangeElemOnChange = function(value) {
     console.log('Gnob Error: No onchange event found! Try adding an onchange event listener to the range element!');
   }
   else {
-   this.rangeElem.onchange(this.rangeElem.value); 
-  }  
+   this.rangeElem.onchange(this.rangeElem.value);
+  }
 };
 
 Gnob.prototype.createKnob = function(rangeElem) {
-  var _this = this;
-  
   var knob = document.createElement('div');
   var inner = document.createElement('div');
   var indicator = document.createElement('div');
   var popover = document.createElement('div');
   var popoverInput = document.createElement('input');
-  
+
   rangeElem.setAttribute('min', this.min);
   rangeElem.setAttribute('max', this.max);
   rangeElem.setAttribute('step', this.step);
-  
+
   this.rangeElem = rangeElem;
-  
+
   knob.classList.add(this.size);
   knob.classList.add('gnob');
   inner.classList.add('inner');
@@ -168,17 +166,17 @@ Gnob.prototype.createKnob = function(rangeElem) {
   inner.appendChild(indicator);
   knob.appendChild(inner);
   knob.appendChild(popover);
-  
+
   if (this.settings.indicator) {
     indicator.style.background = this.settings.indicator;
-    indicator.style.boxShadow  = "0px 0px 8px " +   this.settings.indicator;
+    indicator.style.boxShadow  = '0px 0px 8px ' +   this.settings.indicator;
   }
-  
+
   this.inner = inner;
   this.knobElem = knob;
   this.popover = popover;
   this.input = popoverInput;
-  
+
   this.setRotation(NATURAL_OFF_DEG);
 
   this.bindEvents(knob);
@@ -192,7 +190,7 @@ Gnob.prototype.rotate = function(delta) {
   var rotation  = NATURAL_OFF_DEG + this.position + step;
 
   this.position += (direction + delta);
-  
+
   if (this.position <= NATURAL_OFF_POS) {
     this.position = NATURAL_OFF_POS;
   } else if (this.position >= NATURAL_MAX_POS) {
@@ -208,8 +206,7 @@ Gnob.prototype.getTicks = function() {
   var out = [];
   var ticks = NATURAL_MAX_POS - NATURAL_OFF_POS;
   var betweenTicks = ticks / this.max;
-  var value = 0;
-  
+
   for (var i = 0; i < this.max; i++) {
     out.push(i * betweenTicks);
   }
@@ -225,7 +222,7 @@ Gnob.prototype.tick = function(position) {
   var _this = this;
   var firstTick = _this.ticks[0];
   var lastTick = _this.ticks[_this.ticks.length - 1];
-  var lastTickIndex = _this.ticks.indexOf(lastTick)
+  var lastTickIndex = _this.ticks.indexOf(lastTick);
   var outOfRange = position >= lastTick;
   var isFirstTick = position <= firstTick;
 
@@ -245,7 +242,7 @@ Gnob.prototype.tick = function(position) {
       }
     });
   }
-  
+
   _this.rangeElemOnChange(_this.rangeElem.value);
 };
 
@@ -256,25 +253,25 @@ Gnob.prototype.bindEvents = function(knobElem) {
     _this.rotate(e.deltaY ? e.deltaY : e.deltaX * -1);
     e.preventDefault();
   }, false);
-  
+
   _this.input.onchange = function(e) {
     _this.setValue(this.value);
     _this.popover.classList.remove('open');
   };
-  
+
   _this.input.onblur = function() {
     _this.popover.classList.remove('open');
   };
-  
+
   _this.inner.ondblclick = function() {
     _this.popover.classList.add('open');
     _this.input.focus();
   };
-  
+
   _this.input.onkeydown = function(e) {
     var which = e.keyCode;
     var value = parseInt(_this.input.value, 10);
-    
+
     if (which === 38) {
       _this.setValue(value + 1);
     }
@@ -288,12 +285,3 @@ Gnob.prototype.bindEvents = function(knobElem) {
   };
 };
 
-window.onload = function() {
-  var sliders = document.querySelectorAll('[data-gnob]');
-  
-
-  Array.prototype.slice.call(sliders)
-    .forEach(function(slider) {
-      new Gnob(slider);
-    });
-};
