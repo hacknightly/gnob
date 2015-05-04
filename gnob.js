535c1c0b84f4473a6b32212d99baf654d1a007a2
hacknightly <hacknightly@gmail.com>
2015-05-04 07:43:05 -0600

    making ticks better

--- gnob.js
+++ gnob.js
@@ -1,8 +1,8 @@
 'use strict';
 // Thanks Mozilla! https://developer.mozilla.org/en-US/docs/Web/Events/wheel
-// creates a global 'addWheelListener' method
-// example: addWheelListener( elem, function( e ) { console.log( e.deltaY ); e.preventDefault(); } );
 (function(window, document) {
+  'use strict';
+
   var prefix = '',
     _addEventListener, onwheel, support;
 
@@ -62,90 +62,42 @@
 
     }, useCapture || false);
   }
-
 })(window, document);
+// END Mozilla wheel plugin
 
-var NATURAL_OFF_POS = 0;
 var NATURAL_OFF_DEG = -190;
-var NATURAL_MAX_POS = 295;
+var NATURAL_MAX_DEG = 100;
 
 var Gnob = function(rangeElem) {
-  this.settings = this.getSettings(rangeElem);
-  this.min      = (this.settings.min || 0);
-  this.max      = this.settings.max ? this.settings.max:10;
-  this.step     = (this.settings.step || 1);
-  this.initial  = (this.settings.initial || 0);
-  this.diameter = (this.settings.diameter || 100);
-  this.size     = (this.settings.size || 'medium');
-  this.position = this.initial;
-  this.ticks    = this.getTicks();
+  this.settings  = this.getSettings(rangeElem);
+  this.min       = (this.settings.min || 0);
+  this.max       = this.settings.max ? this.settings.max:10;
+  this.step      = (this.settings.step || 1);
+  this.initial   = (this.settings.initial || 0);
+  this.diameter  = (this.settings.diameter || 100);
+  this.size      = (this.settings.size || 'medium');
+  this.degrees   = NATURAL_OFF_DEG;
+  this.precision = 0;
+
+  if (this.settings.step.toString().indexOf('.') > -1) {
+    this.precision =  this.settings.step
+      .toString()
+      .match(/\.+\d+/)[0]
+      .replace('.', '').length;
+  }
+
+  this.ticks = this.getTicks();
 
   this.createKnob(rangeElem);
 
   this.setValue(this.initial);
 };
 
-Gnob.prototype.getSettings = function(rangeElem) {
-  var options = {};
-
-  Array.prototype.slice
-    .call(rangeElem.attributes)
-    .map(function(attr) {
-      if (attr.name.indexOf('gnob') > -1) {
-        var name = attr.name.replace(/data-gnob-?/, ''),
-          intVal = parseInt(attr.value, 10);
-        if (name) {
-          options[name] = (intVal || intVal === 0) ? intVal : attr.value;
-        }
-      }
-    });
-
-  return options;
-};
-
-Gnob.prototype.setValue = function(value) {
-  value = parseInt(value, 10);
-
-  if (value > this.max) {
-    value = this.max;
-  }
-  else if (value < this.min) {
-    value = this.min;
-  }
-  else if (!value && value !== 0) {
-    return false;
-  }
-
-  var position = NATURAL_OFF_DEG + (value > 0 ? this.ticks[value] : 0);
-
-  this.position = this.ticks[value];
-  this.rangeElem.value = position;
-  this.setRotation(position);
-  this.input.value = value;
-
-  this.rangeElemOnChange(value);
-};
-
-Gnob.prototype.setRotation = function(value) {
-  var inner = this.knobElem.children[0];
-
-  inner.style.transform = 'rotate(' + value + 'deg)';
-};
-
-Gnob.prototype.rangeElemOnChange = function(value) {
-  if (typeof this.rangeElem.onchange !== 'function') {
-    console.log('Gnob Error: No onchange event found! Try adding an onchange event listener to the range element!');
-  }
-  else {
-   this.rangeElem.onchange(this.rangeElem.value);
-  }
-};
-
 Gnob.prototype.createKnob = function(rangeElem) {
-  var knob = document.createElement('div');
-  var inner = document.createElement('div');
-  var indicator = document.createElement('div');
-  var popover = document.createElement('div');
+  var knob         = document.createElement('div');
+  var inner        = document.createElement('div');
+  var indicator    = document.createElement('div');
+  var popover      = document.createElement('div');
   var popoverInput = document.createElement('input');
 
   rangeElem.setAttribute('min', this.min);
@@ -182,68 +134,91 @@ Gnob.prototype.createKnob = function(rangeElem) {
   rangeElem.parentNode.replaceChild(knob, rangeElem);
 };
 
-Gnob.prototype.rotate = function(delta) {
-  var direction = delta > 0 ? 1 : -1;
-  var step      = this.step * direction;
-  var rotation  = NATURAL_OFF_DEG + this.position + step;
+Gnob.prototype.getSettings = function(rangeElem) {
+  var options = {};
 
-  this.position += (direction + delta);
+  Array.prototype.slice
+    .call(rangeElem.attributes)
+    .map(function(attr) {
+      if (attr.name.indexOf('gnob') > -1) {
+        var name = attr.name.replace(/data-gnob-?/, ''),
+          intVal = parseFloat(attr.value, 10);
+        if (name) {
+          options[name] = (intVal || intVal === 0) ? intVal : attr.value;
+        }
+      }
+    });
 
-  if (this.position <= NATURAL_OFF_POS) {
-    this.position = NATURAL_OFF_POS;
-  } else if (this.position >= NATURAL_MAX_POS) {
-    this.position = NATURAL_MAX_POS;
+  return options;
+};
+
+Gnob.prototype.setValue = function(value) {
+  if (value > this.max) {
+    value = this.max;
+  }
+  else if (value < this.min) {
+    value = this.min;
+  }
+  else if (!value && value !== 0) {
+    return false;
   }
 
-  this.tick(this.position);
+  this.rangeElem.value = value;
+  this.input.value     = value;
 
-  this.setRotation(rotation);
+  this.rangeElemOnChange(value);
+};
+
+Gnob.prototype.setRotation = function(value) {
+  var inner = this.knobElem.children[0];
+
+  inner.style.transform = 'rotate(' + value + 'deg)';
+};
+
+Gnob.prototype.rangeElemOnChange = function(value) {
+  if (typeof this.rangeElem.onchange !== 'function') {
+    console.log('Gnob Error: No onchange event found! Try adding an onchange event listener to the range element!');
+  }
+  else {
+   this.rangeElem.onchange(value);
+  }
+};
+
+Gnob.prototype.rotate = function(delta) {
+  this.degrees += delta;
+
+  if (this.degrees <= NATURAL_OFF_DEG) {
+    this.degrees = NATURAL_OFF_DEG;
+  } else if (this.degrees >= NATURAL_MAX_DEG) {
+    this.degrees = NATURAL_MAX_DEG;
+  }
+
+  this.tick(this.degrees);
 };
 
 Gnob.prototype.getTicks = function() {
-  var out = [];
-  var ticks = NATURAL_MAX_POS - NATURAL_OFF_POS;
-  var inclusiveMax = this.max + 1;
+  var ticks          = {};
+  var totalDegrees   = Math.abs(NATURAL_OFF_DEG) + NATURAL_MAX_DEG;
+  var degreesPerTick = (this.step / totalDegrees);
 
-  var betweenTicks = ticks / inclusiveMax;
+  for (var i = 0; i < totalDegrees; i++) {
+    var value = parseFloat((i * degreesPerTick).toFixed(this.precision), 10);
 
-  for (var i = 0; i < inclusiveMax; i++) {
-    out.push(i * betweenTicks);
+    ticks[i] = value;
   }
 
-  out.pop();
-
-  out.push(NATURAL_MAX_POS);
-
-  return out;
+  return ticks;
 };
 
-Gnob.prototype.tick = function(position) {
-  var _this = this;
-  var firstTick = _this.ticks[0];
-  var lastTick = _this.ticks[_this.ticks.length - 1];
-  var lastTickIndex = _this.ticks.indexOf(lastTick);
-  var outOfRange = position >= lastTick;
-  var isFirstTick = position <= firstTick;
+Gnob.prototype.tick = function(degrees) {
+  var distance = Math.floor(Math.abs(NATURAL_OFF_DEG - degrees));
+  var value    = this.ticks[distance];
 
-  if (outOfRange) {
-    _this.rangeElem.value = lastTickIndex;
-    _this.input.value = lastTickIndex;
-  } else if (isFirstTick) {
-    _this.rangeElem.value = position;
-    _this.input.value = position;
-  } else {
-    this.ticks.forEach(function(tick, i) {
-      var inTickRange = position > tick && position < _this.ticks[i + 1];
+  if (degrees === NATURAL_OFF_DEG) { value = this.min; }
+  else if (degrees === NATURAL_MAX_DEG) { value = this.max; }
 
-      if (inTickRange) {
-        _this.rangeElem.value = i;
-        _this.input.value = i;
-      }
-    });
-  }
-
-  _this.rangeElemOnChange(_this.rangeElem.value);
+  this.setRotation(degrees);
+  this.setValue(value);
 };
 
 Gnob.prototype.bindEvents = function(knobElem) {
@@ -255,7 +230,7 @@ Gnob.prototype.bindEvents = function(knobElem) {
   }, false);
 
   _this.input.onchange = function(e) {
-    _this.setValue(this.value);
+    //_this.setValue(this.value);
     _this.popover.classList.remove('open');
   };
 
@@ -264,23 +239,32 @@ Gnob.prototype.bindEvents = function(knobElem) {
   };
 
   _this.inner.ondblclick = function() {
-    _this.popover.classList.add('open');
+    _this.popover.classList.toggle('open');
     _this.input.focus();
   };
 
   _this.input.onkeydown = function(e) {
     var which = e.keyCode;
-    var value = parseInt(_this.input.value, 10);
 
     if (which === 38) {
-      _this.setValue(value + 1);
+      _this.setValue(_this.degrees + (1 * _this.step));
     }
     else if (which === 40) {
-      _this.setValue(value - 1);
+      _this.setValue(_this.degrees - (1 * _this.step));
     }
     else if(which === 13) {
-      _this.setValue(value);
+      _this.setValue(_this.degrees);
       _this.popover.classList.remove('open');
     }
   };
 };
+
+window.onload = function() {
+  var ranges = document.querySelectorAll('[data-gnob]');
+
+  Array.prototype.slice.call(ranges).forEach(function(range) {
+    range.onchange = function(value) { console.log(value); }
+
+    new Gnob(range);
+  });
+}
